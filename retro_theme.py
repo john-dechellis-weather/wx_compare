b8b1d2d9ac727e9829d1638caa2258dfc3c6cc09
john-dechellis-weather <john.dechellis@tomorrow.io>
2026-08-02 20:47:51 -0400

    colors

--- retro_theme.py
+++ retro_theme.py
@@ -286,11 +286,25 @@ input[type="text"], .stTextInput input {
     color: #FFFFFF !important;
 }
 
-/* The popup list of times (rendered separately) */
+/* Dropdown popups (calendar + time list): white background, black text */
+div[data-baseweb="popover"],
+div[data-baseweb="popover"] > div,
+div[data-baseweb="calendar"],
+div[data-baseweb="datepicker"] {
+    background-color: #FFFFFF !important;
+}
 div[data-baseweb="popover"] li,
-div[data-baseweb="popover"] li * {
+div[data-baseweb="popover"] li *,
+div[data-baseweb="calendar"] *,
+div[data-baseweb="datepicker"] * {
+    color: #000000 !important;
+}
+/* Keep the selected day readable */
+div[data-baseweb="calendar"] [aria-selected="true"] {
+    background-color: #000000 !important;
     color: #FFFFFF !important;
 }
+
 </style>
 """
 def apply_retro_theme():
